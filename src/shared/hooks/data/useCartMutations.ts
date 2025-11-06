d04861e65f92aebb77f59eebf12800e91780c4cd
Vinicius <viniciusaugustooliveira2007@gmail.com>
2025-11-06 11:28:36 -0300

    fix(useCreateCart): fix misspelled queryClient variable

--- src/shared/hooks/data/useCartMutations.ts
+++ src/shared/hooks/data/useCartMutations.ts
@@ -3,12 +3,12 @@ import type { CreateCartParams } from "@/shared/types/Params";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export const useCreateCart = () => {
-  const querClient = useQueryClient();
+  const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: (params: CreateCartParams) => cartService.createCart(params),
     onSuccess: () => {
-      querClient.invalidateQueries({ queryKey: ["cart"] });
+      queryClient.invalidateQueries({ queryKey: ["cart"] });
     },
   });
 };
